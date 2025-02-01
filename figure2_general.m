f60298bb941ed5225996fdac5b38ed29ccf002ba
Ramanujan Srinath <8876869+ramanujansrinath@users.noreply.github.com>
2025-02-01 11:14:55 -0600

    marginals for choice corr plot

--- figure2_general.m
+++ figure2_general.m
@@ -3,9 +3,11 @@ clc; clear; close all;
 load('data/figure2_cVals.mat')
 
 % this code also generates supp6
-
 figure; plotGenDec(cVals_v1); sgtitle('V1')
 figure; plotGenDec(cVals_v4); sgtitle('V4')
+
+figure; plotGenDec_marg(cVals_v1); sgtitle('V1')
+figure; plotGenDec_marg(cVals_v4); sgtitle('V4')
 
 %%
 function plotGenDec(cVals)
@@ -44,3 +46,39 @@ function plotGenDec(cVals)
     fixPlot(gca,[-0.04 0.4],[-0.04 0.4],'avg beh error for worse dec','avg beh error for better dec',0:0.2:1,0:0.2:1,{'avg beh error for better' 'and worse decoded shapes'})
 end
 
+function plotGenDec_marg(cVals)
+    x = cVals(:,4);
+    y = cVals(:,6);
+    t = cVals(:,8);
+
+    cols = {[0 0 0] [0.5 0.25 0.5] [0.4 0.6 0.6]};
+    
+    set(gcf,'color','w','Position',[-1597 215 822 764])
+    subplot(221); hold on;
+    histogram(x(t==1),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{1},'LineWidth',2);
+    histogram(x(t==2),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{2},'LineWidth',2);
+    histogram(x(t==3),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{3},'LineWidth',2);
+    line([mean(x(t==1)) mean(x(t==1))],[50 120],'linewidth',2,'color',cols{1},'linestyle','--')
+    line([mean(x(t==2)) mean(x(t==2))],[50 120],'linewidth',2,'color',cols{2},'linestyle','--')
+    line([mean(x(t==3)) mean(x(t==3))],[50 120],'linewidth',2,'color',cols{3},'linestyle','--')
+    line([0 0],[0 120],'linewidth',2,'color','k','linestyle','--')
+    fixPlot(gca,[-0.1 1],[0 40],'','',-1:0.5:1,0:30:200); set(gca,'XTickLabel',{})
+    
+    subplot(224); hold on;
+    histogram(y(t==1),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{1},'LineWidth',2);
+    histogram(y(t==2),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{2},'LineWidth',2);
+    histogram(y(t==3),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{3},'LineWidth',2);
+    line([mean(y(t==1)) mean(y(t==1))],[50 120],'linewidth',2,'color',cols{1},'linestyle','--')
+    line([mean(y(t==2)) mean(y(t==2))],[50 120],'linewidth',2,'color',cols{2},'linestyle','--')
+    line([mean(y(t==3)) mean(y(t==3))],[50 120],'linewidth',2,'color',cols{3},'linestyle','--')
+    line([0 0],[0 120],'linewidth',2,'color','k','linestyle','--')
+    view([90 -90])
+    fixPlot(gca,[-0.1 1],[0 40],'','',-1:0.5:1,0:30:200); set(gca,'XTickLabel',{})
+    
+    subplot(223); hold on;
+    plot(x(t==1),y(t==1),'o','MarkerSize',7,'MarkerFaceColor','w','LineWidth',2,'color',cols{1})
+    plot(x(t==2),y(t==2),'o','MarkerSize',7,'MarkerFaceColor','w','LineWidth',2,'color',cols{2})
+    plot(x(t==3),y(t==3),'o','MarkerSize',7,'MarkerFaceColor','w','LineWidth',2,'color',cols{3})
+    line([-1 1],[-1 1],'color','k','linestyle','--','linewidth',2);
+    fixPlot(gca,[-0.1 1],[-0.1 1],'shape specific','shape general',-1:0.25:1,-1:0.25:1)
+end
