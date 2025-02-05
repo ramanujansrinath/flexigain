828e576aee575c2af46ef822f8ac94f9b644d94e
Ramanujan Srinath <8876869+ramanujansrinath@users.noreply.github.com>
2025-02-04 18:34:02 -0600

    Making the gen dec panel...
    ... consistent with the error panel.

--- figure2_general.m
+++ figure2_general.m
@@ -58,9 +58,9 @@ function plotGenDec_marg(cVals)
     histogram(x(t==1),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{1},'LineWidth',2);
     histogram(x(t==2),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{2},'LineWidth',2);
     histogram(x(t==3),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{3},'LineWidth',2);
-    line([mean(x(t==1)) mean(x(t==1))],[50 120],'linewidth',2,'color',cols{1},'linestyle','--')
-    line([mean(x(t==2)) mean(x(t==2))],[50 120],'linewidth',2,'color',cols{2},'linestyle','--')
-    line([mean(x(t==3)) mean(x(t==3))],[50 120],'linewidth',2,'color',cols{3},'linestyle','--')
+    line([mean(x(t==1)) mean(x(t==1))],[30 120],'linewidth',2,'color',cols{1},'linestyle','--')
+    line([mean(x(t==2)) mean(x(t==2))],[30 120],'linewidth',2,'color',cols{2},'linestyle','--')
+    line([mean(x(t==3)) mean(x(t==3))],[30 120],'linewidth',2,'color',cols{3},'linestyle','--')
     line([0 0],[0 120],'linewidth',2,'color','k','linestyle','--')
     fixPlot(gca,[-0.1 1],[0 40],'','',-1:0.5:1,0:30:200); set(gca,'XTickLabel',{})
     
@@ -68,17 +68,17 @@ function plotGenDec_marg(cVals)
     histogram(y(t==1),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{1},'LineWidth',2);
     histogram(y(t==2),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{2},'LineWidth',2);
     histogram(y(t==3),linspace(-0.1,1,20),'DisplayStyle','stairs','EdgeColor',cols{3},'LineWidth',2);
-    line([mean(y(t==1)) mean(y(t==1))],[50 120],'linewidth',2,'color',cols{1},'linestyle','--')
-    line([mean(y(t==2)) mean(y(t==2))],[50 120],'linewidth',2,'color',cols{2},'linestyle','--')
-    line([mean(y(t==3)) mean(y(t==3))],[50 120],'linewidth',2,'color',cols{3},'linestyle','--')
+    line([mean(y(t==1)) mean(y(t==1))],[30 120],'linewidth',2,'color',cols{1},'linestyle','--')
+    line([mean(y(t==2)) mean(y(t==2))],[30 120],'linewidth',2,'color',cols{2},'linestyle','--')
+    line([mean(y(t==3)) mean(y(t==3))],[30 120],'linewidth',2,'color',cols{3},'linestyle','--')
     line([0 0],[0 120],'linewidth',2,'color','k','linestyle','--')
     view([90 -90])
     fixPlot(gca,[-0.1 1],[0 40],'','',-1:0.5:1,0:30:200); set(gca,'XTickLabel',{})
     
     subplot(223); hold on;
-    plot(x(t==1),y(t==1),'o','MarkerSize',7,'MarkerFaceColor','w','LineWidth',2,'color',cols{1})
-    plot(x(t==2),y(t==2),'o','MarkerSize',7,'MarkerFaceColor','w','LineWidth',2,'color',cols{2})
-    plot(x(t==3),y(t==3),'o','MarkerSize',7,'MarkerFaceColor','w','LineWidth',2,'color',cols{3})
+    plot(x(t==1),y(t==1),'.','MarkerSize',12,'MarkerFaceColor','w','LineWidth',2,'color',cols{1})
+    plot(x(t==2),y(t==2),'.','MarkerSize',12,'MarkerFaceColor','w','LineWidth',2,'color',cols{2})
+    plot(x(t==3),y(t==3),'.','MarkerSize',12,'MarkerFaceColor','w','LineWidth',2,'color',cols{3})
     line([-1 1],[-1 1],'color','k','linestyle','--','linewidth',2);
     fixPlot(gca,[-0.1 1],[-0.1 1],'shape specific','shape general',-1:0.25:1,-1:0.25:1)
 end
